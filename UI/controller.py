3402c4d5144eacd6bc12ac9076eb6778915427c2
JosephAv <g.averta3@gmail.com>
2026-06-26 14:39:31 +0200

    first commit

---
+++ UI/controller.py
@@ -0,0 +1,22 @@
+import flet as ft
+
+
+class Controller:
+    def __init__(self, view, model):
+        # the view, with the graphical elements of the UI
+        self._view = view
+        # the model, which implements the logic of the program and holds the data
+        self._model = model
+
+
+    def handleCreaGrafo(self, e):
+        pass
+
+    def handleStampaInfo(self,e):
+        pass
+
+    def handleStampaDettagli(self,e):
+        pass
+
+    def handleSequenza(self,e):
+        pass
