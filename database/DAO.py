4ff029bf3ddcbad6036f999e572e440c6b1f85a8
JosephAv <g.averta3@gmail.com>
2026-06-27 17:27:54 +0200

    add a template dao method

--- database/DAO.py
+++ database/DAO.py
@@ -2,5 +2,22 @@ from database.DB_connect import DBConnect
 
 
 class DAO():
-    def __init__(self):
-        pass
+    @staticmethod
+    def getAllAlbums():
+        conn = DBConnect.get_connection()
+
+        results = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """
+                select * from album
+                """
+
+        cursor.execute(query)
+
+        for row in cursor:
+            results.append(**row)
+
+        cursor.close()
+        conn.close()
+        return results
